d9c4e18468ee64675e3d6f49fe63367ef05a5930
Shaik Jakeer Hussain <127141771+Jvjjggj@users.noreply.github.com>
2023-08-22 20:00:43 +0530

    completed upto api 6

--- app.js
+++ app.js
@@ -21,7 +21,13 @@ const connectDBAndServer = async () => {
 };
 
 connectDBAndServer();
-
+const matchdetails = function (i) {
+  return {
+    matchId: i.match_id,
+    match: i.match,
+    year: i.year,
+  };
+};
 const playerDetailsFormat = (i) => {
   return {
     playerId: i.player_id,
@@ -78,14 +84,73 @@ app.get("/matches/:matchId/", async (request, response) => {
     where 
        match_id=${matchId} ;`;
   const dbresponse = await db.get(query);
-  response.send(dbresponse);
+  const change = matchdetails(dbresponse);
+  response.send(change);
 });
 
 // API 5  https://github.com/Jvjjggj/playerMatchDetails.git
 app.get("/players/:playerId/matches", async (request, response) => {
   const { playerId } = request.params;
   const query = `
-    select * from 
+    select * from match_details natural join player_match_score
+    where 
+       player_id=${playerId}
     `;
+  const format = function (i) {
+    return {
+      matchId: i.match_id,
+      match: i.match,
+      year: i.year,
+    };
+  };
+  const dbresponse = await db.all(query);
+  let lst = [];
+  for (let i of dbresponse) {
+    lst.push(format(i));
+  }
+  response.send(lst);
 });
+
+// API 6
+app.get("/matches/:matchId/players", async (request, response) => {
+  const { matchId } = request.params;
+  const query = `
+  select * 
+  from player_details inner join player_match_score on (player_details.player_id=player_match_score.player_id)
+  where 
+     match_id=${matchId};`;
+
+  const dbresponse = await db.all(query);
+  const format = (i) => {
+    return {
+      playerId: i.player_id,
+      playerName: i.player_name,
+    };
+  };
+  const lst = [];
+  for (let i of dbresponse) {
+    lst.push(format(i));
+  }
+  response.send(lst);
+});
+
+// API 7
+
+app.get("/players/:playerId/playerScores", async (request, response) => {
+  const { playerId } = request.params;
+  const query = `
+  select 
+     player_id as playerId,
+     player_name as playerName,
+     sum(score) as score,
+     sum(fours) as fours,
+     sum(sixes) as sixes
+  from 
+  player_match_score natural join player_details
+  where
+     player_id=${playerId};`;
+  const dbresponse = await db.all(query);
+  response.send(dbresponse);
+});
+
 module.exports = app;
